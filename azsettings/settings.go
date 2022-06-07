cce842b82e33d6cd2d33605ee3c0de9ba944a59a
Sergey Kostrukov <sekost@microsoft.com>
2022-06-06 18:43:58 -0700

    Configuration for user identity authentication

--- azsettings/settings.go
+++ azsettings/settings.go
@@ -4,6 +4,15 @@ type AzureSettings struct {
 	Cloud                   string
 	ManagedIdentityEnabled  bool
 	ManagedIdentityClientId string
+
+	UserIdentityEnabled       bool
+	UserIdentityTokenEndpoint *TokenEndpointSettings
+}
+
+type TokenEndpointSettings struct {
+	TokenUrl     string
+	ClientId     string
+	ClientSecret string
 }
 
 func (settings *AzureSettings) GetDefaultCloud() string {
