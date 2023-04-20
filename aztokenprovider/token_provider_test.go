ca9f6cba1c9190003ebc4cdbd83c3c9bcf9be387
Sergey Kostrukov <sekost@microsoft.com>
2023-04-20 08:02:27 -0700

    Unit tests

--- aztokenprovider/token_provider_test.go
+++ aztokenprovider/token_provider_test.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/grafana/grafana-azure-sdk-go/azcredentials"
 	"github.com/grafana/grafana-azure-sdk-go/azsettings"
+	"github.com/grafana/grafana-azure-sdk-go/azusercontext"
+	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -19,7 +21,7 @@ func (c *tokenCacheFake) GetAccessToken(_ context.Context, credential TokenRetri
 	return "4cb83b87-0ffb-4abd-82f6-48a8c08afc53", nil
 }
 
-func TestAzureTokenProvider_GetAccessToken(t *testing.T) {
+func TestNewAzureAccessTokenProvider_ServiceIdentity(t *testing.T) {
 	ctx := context.Background()
 
 	settings := &azsettings.AzureSettings{}
@@ -38,7 +40,7 @@ func TestAzureTokenProvider_GetAccessToken(t *testing.T) {
 		t.Run("should resolve managed identity retriever if auth type is managed identity", func(t *testing.T) {
 			credentials := &azcredentials.AzureManagedIdentityCredentials{}
 
-			provider, err := NewAzureAccessTokenProvider(settings, credentials)
+			provider, err := NewAzureAccessTokenProvider(settings, credentials, false)
 			require.NoError(t, err)
 			require.IsType(t, &serviceTokenProvider{}, provider)
 
@@ -53,7 +55,7 @@ func TestAzureTokenProvider_GetAccessToken(t *testing.T) {
 		t.Run("should resolve client secret retriever if auth type is client secret", func(t *testing.T) {
 			credentials := &azcredentials.AzureClientSecretCredentials{AzureCloud: azsettings.AzurePublic}
 
-			provider, err := NewAzureAccessTokenProvider(settings, credentials)
+			provider, err := NewAzureAccessTokenProvider(settings, credentials, false)
 			require.NoError(t, err)
 			require.IsType(t, &serviceTokenProvider{}, provider)
 
@@ -72,8 +74,140 @@ func TestAzureTokenProvider_GetAccessToken(t *testing.T) {
 		t.Run("should return error if auth type is managed identity", func(t *testing.T) {
 			credentials := &azcredentials.AzureManagedIdentityCredentials{}
 
-			_, err := NewAzureAccessTokenProvider(settings, credentials)
+			_, err := NewAzureAccessTokenProvider(settings, credentials, false)
 			assert.Error(t, err, "managed identity authentication is not enabled in Grafana config")
 		})
+	})
+}
+
+func TestNewAzureAccessTokenProvider_UserIdentity(t *testing.T) {
+	settingsNotConfigured := &azsettings.AzureSettings{}
+
+	settings := &azsettings.AzureSettings{
+		UserIdentityEnabled: true,
+		UserIdentityTokenEndpoint: &azsettings.TokenEndpointSettings{
+			TokenUrl:     "FAKE_TOKEN_URL",
+			ClientId:     "FAKE_CLIENT_ID",
+			ClientSecret: "FAKE_CLIENT_SECRET",
+		},
+	}
+
+	t.Run("should fail when user identity not supported", func(t *testing.T) {
+		credentials := &azcredentials.AadCurrentUserCredentials{}
+
+		_, err := NewAzureAccessTokenProvider(settingsNotConfigured, credentials, false)
+		assert.Error(t, err)
+	})
+
+	t.Run("should fail when user identity not configured", func(t *testing.T) {
+		credentials := &azcredentials.AadCurrentUserCredentials{}
+
+		_, err := NewAzureAccessTokenProvider(settingsNotConfigured, credentials, true)
+		assert.Error(t, err)
+	})
+
+	t.Run("should return user provider when user identity configured", func(t *testing.T) {
+		credentials := &azcredentials.AadCurrentUserCredentials{}
+
+		provider, err := NewAzureAccessTokenProvider(settings, credentials, true)
+		require.NoError(t, err)
+		require.IsType(t, &userTokenProvider{}, provider)
+	})
+}
+
+func TestGetAccessToken_UserIdentity(t *testing.T) {
+	ctx := context.Background()
+
+	scopes := []string{
+		"https://management.azure.com/.default",
+	}
+
+	var err error
+
+	t.Run("should fail if user context not configured", func(t *testing.T) {
+		var provider AzureTokenProvider = &userTokenProvider{
+			tokenCache: &tokenCacheFake{},
+		}
+
+		getAccessTokenFunc = func(retriever TokenRetriever, scopes []string) {
+			assert.IsType(t, &onBehalfOfTokenRetriever{}, retriever)
+		}
+
+		_, err = provider.GetAccessToken(ctx, scopes)
+		assert.Error(t, err)
+	})
+
+	t.Run("should fail if no user in user context", func(t *testing.T) {
+		var provider AzureTokenProvider = &userTokenProvider{
+			tokenCache: &tokenCacheFake{},
+		}
+
+		getAccessTokenFunc = func(retriever TokenRetriever, scopes []string) {
+			assert.IsType(t, &onBehalfOfTokenRetriever{}, retriever)
+		}
+
+		usrctx := azusercontext.WithCurrentUser(ctx, azusercontext.CurrentUserContext{})
+
+		_, err = provider.GetAccessToken(usrctx, scopes)
+		assert.Error(t, err)
+	})
+
+	t.Run("should fail if no ID token in user context", func(t *testing.T) {
+		var provider AzureTokenProvider = &userTokenProvider{
+			tokenCache: &tokenCacheFake{},
+		}
+
+		getAccessTokenFunc = func(retriever TokenRetriever, scopes []string) {
+			assert.IsType(t, &onBehalfOfTokenRetriever{}, retriever)
+		}
+
+		usrctx := azusercontext.WithCurrentUser(ctx, azusercontext.CurrentUserContext{
+			User: &backend.User{
+				Login: "user1@example.org",
+			},
+		})
+
+		_, err = provider.GetAccessToken(usrctx, scopes)
+		assert.Error(t, err)
+	})
+
+	t.Run("should use onBehalfOfTokenRetriever by default", func(t *testing.T) {
+		var provider AzureTokenProvider = &userTokenProvider{
+			tokenCache: &tokenCacheFake{},
+		}
+
+		getAccessTokenFunc = func(retriever TokenRetriever, scopes []string) {
+			assert.IsType(t, &onBehalfOfTokenRetriever{}, retriever)
+		}
+
+		usrctx := azusercontext.WithCurrentUser(ctx, azusercontext.CurrentUserContext{
+			User: &backend.User{
+				Login: "user1@example.org",
+			},
+			IdToken: "FAKE_ID_TOKEN",
+		})
+
+		_, err = provider.GetAccessToken(usrctx, scopes)
+		require.NoError(t, err)
+	})
+
+	t.Run("should use usernameTokenRetriever for usernameAssertion", func(t *testing.T) {
+		var provider AzureTokenProvider = &userTokenProvider{
+			tokenCache:        &tokenCacheFake{},
+			usernameAssertion: true,
+		}
+
+		getAccessTokenFunc = func(retriever TokenRetriever, scopes []string) {
+			assert.IsType(t, &usernameTokenRetriever{}, retriever)
+		}
+
+		usrctx := azusercontext.WithCurrentUser(ctx, azusercontext.CurrentUserContext{
+			User: &backend.User{
+				Login: "user1@example.org",
+			},
+		})
+
+		_, err = provider.GetAccessToken(usrctx, scopes)
+		require.NoError(t, err)
 	})
 }
