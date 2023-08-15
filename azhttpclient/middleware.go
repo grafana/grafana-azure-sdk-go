d81fa544d4615a7f9081275bb85880e8bdc32cc5
Sergey Kostrukov <sekost@microsoft.com>
2023-08-14 17:18:50 -0700

    Rate limit sessions for throttling control

--- azhttpclient/middleware.go
+++ azhttpclient/middleware.go
@@ -17,6 +17,7 @@ func AzureMiddleware(authOpts *AuthOptions, credentials azcredentials.AzureCrede
 	return httpclient.NamedMiddlewareFunc(azureMiddlewareName, func(clientOpts httpclient.Options, next http.RoundTripper) http.RoundTripper {
 		var err error
 		var tokenProvider aztokenprovider.AzureTokenProvider = nil
+		var sessionProvider *userSessionProvider = nil
 
 		if tokenProviderFactory, ok := authOpts.customProviders[credentials.AzureAuthType()]; ok && tokenProviderFactory != nil {
 			tokenProvider, err = tokenProviderFactory(authOpts.settings, credentials)
@@ -27,18 +28,27 @@ func AzureMiddleware(authOpts *AuthOptions, credentials azcredentials.AzureCrede
 			return errorResponse(err)
 		}
 
+		if authOpts.rateLimitSession {
+			sessionProvider, err = newSessionProvider()
+			if err != nil {
+				return errorResponse(err)
+			}
+		}
+
 		if len(authOpts.scopes) == 0 {
 			err = errors.New("scopes not configured")
 			return errorResponse(err)
 		}
 
-		return applyAzureAuth(tokenProvider, authOpts.scopes, authOpts.endpoints, next)
+		return applyAzureAuth(tokenProvider, sessionProvider, authOpts.scopes, authOpts.endpoints, next)
 	})
 }
 
-func applyAzureAuth(tokenProvider aztokenprovider.AzureTokenProvider, scopes []string,
-	endpoints *azendpoint.EndpointAllowlist, next http.RoundTripper) http.RoundTripper {
+func applyAzureAuth(tokenProvider aztokenprovider.AzureTokenProvider, sessionProvider *userSessionProvider,
+	scopes []string, endpoints *azendpoint.EndpointAllowlist, next http.RoundTripper) http.RoundTripper {
 	return httpclient.RoundTripperFunc(func(req *http.Request) (*http.Response, error) {
+		reqContext := req.Context()
+
 		if endpoints != nil {
 			endpoint := azendpoint.Endpoint(*req.URL)
 			if !endpoints.IsAllowed(endpoint) {
@@ -46,11 +56,20 @@ func applyAzureAuth(tokenProvider aztokenprovider.AzureTokenProvider, scopes []s
 			}
 		}
 
-		token, err := tokenProvider.GetAccessToken(req.Context(), scopes)
+		token, err := tokenProvider.GetAccessToken(reqContext, scopes)
 		if err != nil {
 			return nil, fmt.Errorf("failed to retrieve Azure access token: %w", err)
 		}
 		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+
+		if sessionProvider != nil {
+			sessionId, err := sessionProvider.GetSessionId(reqContext)
+			if err != nil {
+				return nil, fmt.Errorf("failed to obtain user session: %w", err)
+			} else if sessionId != "" {
+				req.Header.Set("X-RateLimit-Session", sessionId)
+			}
+		}
 
 		return next.RoundTrip(req)
 	})
